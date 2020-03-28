26d3ac25993e7edf32fd47dc057e1ebc0bcd678d
AFlachs <62775125+AFlachs@users.noreply.github.com>
2020-03-28 12:58:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Main file of the applet showing a wave paquet going through a potential barrier
+#This is a bonus part of the PHYS-H2001 course 
