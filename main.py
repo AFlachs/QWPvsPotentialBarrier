c5aea95e39209b107683f20fb9a8d4c128535c7e
AFlachs <alexandre.flachs@ulb.be>
2020-03-28 13:30:05 +0100

    Ajoute hello world

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Main file of the applet showing a wave paquet going through a potential barrier
-#This is a bonus part of the PHYS-H2001 course 
+#This is a bonus part of the PHYS-H2001 course
+
+print("hello world")
