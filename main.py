21d2eb2de7d34eac20ea5a3fc67a1b97928f778b
Alex <alexandre.flachs@ulb.be>
2020-03-30 18:07:28 +0200

    Class Wave added in main, from pycharm

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Main file of the applet showing a wave paquet going through a potential barrier
-#This is a bonus part of the PHYS-H2001 course
 
-print("hello world")
+# Main file of the applet showing a wave packet going through a potential barrier
+# This is a bonus part of the PHYS-H2001 course
+
+
+class Wave:
+    def __init__(self):
+        self.k = 1
+        self.omega = 1
+        self.wave = 0
+
+    def wave(self, x, t):
+        return np.exp(1j * (k * x - omega * t))
+
+
+
